6d3a8f78488789fcff526dda94878a7557175d4f
Roy Ho <kwanyin2000@gmail.com>
2020-12-20 18:10:56 +0800

    Section 2: Project 1 - 6 Class components

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,35 @@
 import React from 'react'
 
 // functional component - arrow function
-const About = () => {
-    const message = 'Hello world'
-    return (
-        <h1>Hello about - {message}</h1>
-    )
-}
+// when to use:
+// 1. for small components
+// 2. reusable component
+// 3. presentional components, pattialy right, we can use HOOKS and specify state
+// const About = () => {
+//     const message = 'Hello world'
+//     return (
+//         <h1>Hello about - {message}</h1>
+//     )
+// }
 
 // const About = () => {
 //     const message = 'Hello world'
 //     return React.createElement('h1', null, 'I am generating this with createElement')
 // }
 
-// unfunctional component - normal function
+// functional component - normal function
 // function About() {
 //     return (
 //         <h1>Hello about page</h1>
 //     )
 // }
 
-
+class About extends React.Component {
+    render() {
+        return (
+            <h1>Hello I am class component</h1>
+        )
+    }
+}
 
 export default About
